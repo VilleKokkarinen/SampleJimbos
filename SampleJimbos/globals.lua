cc28ea51fa8b76ee1eeb7b45b16dea9e0f1cfa25
Ville Kokkarinen <Ville.Kokkarinen@ecomond.com>
2025-06-15 19:48:04 +0300

    update to match new steamodded version

---
+++ SampleJimbos/globals.lua
@@ -0,0 +1,35 @@
+--- GLOBALS
+
+G.C.SJ = {
+    RED = HEX("FF0000"),
+    BLACK = HEX("000000"),
+    BLUE = HEX("0000FF"),
+    GREEN = HEX("00FF00"),
+    WHITE = HEX("FFFFFF"),
+    TRANSPARENT = HEX("00000000"),
+}
+
+-- Hooks
+
+local loc_colour_ref = loc_colour
+function loc_colour(_c, _default)
+    if not G.ARGS.LOC_COLOURS then
+        loc_colour_ref()
+    end
+    G.ARGS.LOC_COLOURS.sj_red = G.C.SJ.RED
+    G.ARGS.LOC_COLOURS.sj_black = G.C.SJ.BLACK
+    G.ARGS.LOC_COLOURS.sj_blue = G.C.SJ.BLUE
+    G.ARGS.LOC_COLOURS.sj_green = G.C.SJ.GREEN
+    G.ARGS.LOC_COLOURS.sj_white = G.C.SJ.WHITE
+    G.ARGS.LOC_COLOURS.sj_transparent = G.C.SJ.TRANSPARENT
+    return loc_colour_ref(_c, _default)
+end
+--[[
+local game_start_run_ref = Game.start_run
+function Game:start_run(args)
+    game_start_run_ref(self, args)
+
+    --self.GAME.sj_create_card = SampleJimbos.debug and SampleJimbos.debug_shop_cards or self.GAME.sj_create_card or {}
+    --SampleJimbos.cards_to_create = self.GAME.sj_create_card
+end
+]]
